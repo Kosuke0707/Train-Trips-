ef76f9dd5b64756665a4d67f17c74ec91bfd4368
Kosuke0707 <juvatkarshubham33@gmail.com>
2024-04-01 07:20:29 +0530

    First Commit

---
+++ app/src/main/java/com/example/myapplication/User.java
@@ -0,0 +1,53 @@
+package com.example.myapplication;
+public class User {
+    private String name;
+    private String email;
+    private String phoneNumber;
+    private String gender;
+
+    // Empty constructor (required by Firebase)
+    public User() {
+    }
+
+    public User(String name, String email, String phoneNumber, String gender) {
+        this.name = name;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.gender = gender;
+    }
+
+    // Getters and setters
+    // You can generate them automatically in most IDEs
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+}
